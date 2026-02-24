67466d9cc6437c1bf8b627abe70b3d596d92fb96
Dimassputra1616 <dimassputra1616@gmail.com>
2026-02-24 13:19:50 +0700

    Configure proxy trust and force HTTPS for subdomain

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => AdminMiddleware::class,
             'asset.sync' => VerifyAssetSyncToken::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
 
     ->withExceptions(function (Exceptions $exceptions) {
